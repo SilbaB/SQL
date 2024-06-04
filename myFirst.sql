b179f638933d8904915d194bb18e6f81e265e6c0
SilbaB <murkomenbasil@gmail.com>
2024-06-04 21:59:10 +0300

    use of join

--- myFirst.sql
+++ myFirst.sql
@@ -15,7 +15,7 @@ WHERE points > 2000;
 
 SELECT *
 FROM customers
-WHERE first_name LIKE 'J%e';
+WHERE first_name LIKE 'J%';
 
 SELECT *
 FROM customers;
@@ -31,3 +31,69 @@ WHERE points between 900 AND 2000;
 SELECT *,unit_price * quantity AS total_price
 FROM order_items
 WHERE order_id > 6 OR unit_price * quantity >  40;
+
+SELECT first_name,last_name
+FROM customers 
+WHERE city IN ( 'Nairobi','Cape');
+
+SELECT *
+FROM customers
+WHERE first_name REGEXP 'Jo';
+
+SELECT *,quantity * unit_price AS total_price
+from order_items
+WHERE order_id =2
+ORDER BY quantity *unit_price DESC
+LIMIT 2;
+
+SELECT 
+   first_name || ' ' || last_name,
+   city
+FROM 
+   customers;
+   SELECT first_name,state
+   FROM customers;
+   
+   SELECT *
+   FROM jumiaa_invoicing.invoices;
+   
+   select SUM(invoice_total) AS invoice_sum
+   FROM jumiaa_invoicing.invoices;
+  
+  SELECT MAX(invoice_total) AS invoice_max, MIN(invoice_total) AS invoice_min
+  FROM jumiaa_invoicing.invoices;
+  SELECT COUNT(payment_date) AS total_amount_payment_date
+  FROM jumiaa_invoicing.invoices;
+  
+  SELECT ROUND(invoice_total,1) AS invoice_total_1dp
+  FROM jumiaa_invoicing.invoices; #it rounds into 1 dp.the round fxn takes two parameters
+   
+   select *
+   FROM jumiaa_invoicing.invoices;
+   
+    SELECT clIent_id, MAX(invoice_total)
+    FROM jumiaa_invoicing.invoices
+    GROUP BY client_id;
+    
+SELECT client_id, AVG(invoice_total) AS invoice_avg
+FROM jumiaa_invoicing.invoices
+GROUP BY CLIENT_ID
+HAVING AVG(INVOICE_TOTAL);
+    
+SELECT *
+FROM customers;
+
+SELECT *
+FROM orders;
+
+#Joininng methods
+
+SELECT *
+FROM orders
+JOIN customers
+on orders.customer_id=customers.customer_id;
+
+SELECT *
+FROM orders
+LEFT JOIN customers
+ON orders.customer_id = customers.customer_id
